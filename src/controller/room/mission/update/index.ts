9b67e0ae01a4d160f77851de79afcc06abe3e183
x3nny <xenny@vip.qq.com>
2025-02-05 12:56:12 +0800

    🔧修改中央资源管理任务

--- src/controller/room/mission/update/index.ts
+++ src/controller/room/mission/update/index.ts
@@ -1,4 +1,5 @@
 import { updateBuildMission } from './buildMission';
+import { updateManageMission } from './manageMission';
 import { updateRepairMission, updateWallRepairMission } from './repairMission';
 import { updateSpawnMission } from './spawnMission';
 import { updateTransportMission } from './transportMission';
@@ -9,6 +10,9 @@ export const roomMissionUpdate = (room: Room) => {
     if (Game.time % 10 === 0) updateSpawnMission(room);
     if (Game.time % 20 === 0) updateTransportMission(room);
     
+    // 中央搬运任务
+    if (Game.time % 30 === 1) updateManageMission(room);
+
     // 建造&修复任务
     if (Game.time % 50 === 1) {
         updateBuildMission(room);
