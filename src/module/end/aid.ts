e99b79ea79aae39b8ac72be674fba5c07ed41ceb
x3nny <xenny@vip.qq.com>
2025-02-05 12:58:35 +0800

    🔧援助旗帜检查

--- src/module/end/aid.ts
+++ src/module/end/aid.ts
@@ -25,5 +25,29 @@ export const endAidCheck = () => {
             })
             Game.flags[flag].memory['lastTime'] = Game.time;
         }
+
+        else if (flag.startsWith('AID-RESOURCE')) {
+            if (Game.time - (Game.flags[flag].memory['lastTime']||0) < 500) continue;
+
+            const spawnRoomName = flag.match(/\[([EW]\d+[NS]\d+)\]/)?.[1];
+            const spawnRoom = Game.rooms[spawnRoomName];
+            if (!spawnRoom || !spawnRoom.my) {
+                Game.flags[flag].remove();
+                continue;
+            }
+
+            const targetRoom = Game.flags[flag].pos.roomName;
+            let sourceRoom = flag.match(/-S\[([EW]\d+[NS]\d+)\]/)?.[1];
+            if (!sourceRoom) sourceRoom = spawnRoomName;
+            let rType = flag.match(/-R\[.+?\]/)?.[1];
+            if (!rType) rType = RESOURCE_ENERGY;
+            addMission(spawnRoom, MISSION_TYPE.SPAWN, SPAWN_MISSION.aid_carrier, {
+                home: spawnRoom.name,
+                sourceRoom: sourceRoom,
+                targetRoom: targetRoom,
+                rType: rType
+            })
+            Game.flags[flag].memory['lastTime'] = Game.time;
+        }
     }
 }
