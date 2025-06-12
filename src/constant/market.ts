42fb33efe268205f9c9957e042fd5098aaac5ec4
X3NNY <xenny@vip.qq.com>
2025-06-12 16:44:10 +0800

    ✨新增市场资源价格界限

---
+++ src/constant/market.ts
@@ -0,0 +1,27 @@
+// 最小卖出价格&最大买入价格
+export const ORDER_PRICE = {
+    [RESOURCE_MIST]: {
+        min: 1300,
+        max: 1300
+    },
+    [RESOURCE_CONCENTRATE]: {
+        min: 225000,
+        max: 215000
+    },
+    [RESOURCE_EXTRACT]: {
+        min: 1350000,
+        max: 1250000
+    },
+    [RESOURCE_SPIRIT]: {
+        min: 4300000,
+        max: 4150000
+    },
+    [RESOURCE_EMANATION]: {
+        min: 11500000,
+        max: 11000000
+    },
+    [RESOURCE_ESSENCE]: {
+        min: 26000000,
+        max: 25000000
+    }
+}
